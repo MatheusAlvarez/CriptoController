c20c4442dbc50b425c0059ec7818347c48e5081e
Matheus Maia Alvarez <88752578+MatheusAlvarez@users.noreply.github.com>
2022-05-13 10:20:01 -0300

    Update MthAlvarez.cs

--- MthAlvarez.cs
+++ MthAlvarez.cs
@@ -9,7 +9,7 @@ namespace MthAlvarez
     static class Program
     {
         /// <summary>
-        /// Punto de entrada principal para la aplicación.
+        /// 
         /// </summary>
         [STAThread]
         static void Main()
